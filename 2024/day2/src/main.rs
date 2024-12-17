ec0a7b0af2d5c39e9d39a3465fe5bbb1c20d6d32
Kirill Belolipetsky <archhaze24@gmail.com>
2024-12-17 14:15:31 +0300

    2024 day 2

--- 2024/day2/src/main.rs
+++ 2024/day2/src/main.rs
@@ -8,29 +8,69 @@ fn main() {
         .read_to_string(&mut file_str)
         .unwrap();
 
+    // part one
     let mut counter = 0;
 
-    for line in file_str.lines() {
-        let numbers: Vec<i32> = line.split_whitespace().map(|v| v.parse().unwrap()).collect();
-
-        let mut diff = numbers[1] - numbers[0];
-        let mut death = false;
-        'inner: for i in 2..numbers.len() {
-            let current_diff = numbers[i] - numbers[i-1];
-
-
-            if (current_diff - diff).abs() > 1 || current_diff - diff == 0 {
-                death = true;
-                break 'inner
-            } else {
-                diff = current_diff
-            }
-        }
-
-        if !death {
+    let reports: Vec<Vec<i32>> = file_str.lines().map(|line| line.split(" ").map(|value| value.parse().unwrap()).collect()).collect();
+    
+    for report in &reports {
+        let max_diff = check_diff(report, None);
+        
+        if max_diff < 4 && max_diff > -4 && max_diff != 0 {
             counter += 1
         }
     }
 
-    println!("{}", counter)
+    println!("part one: {}", counter);
+    
+    // part two
+
+    let mut counter = 0;
+    
+    'outer: for report in &reports {
+        let max_diff = check_diff(report, None);
+
+
+        if max_diff < 4 && max_diff > -4 && max_diff != 0 {
+            counter += 1
+        } else {
+            for skip in 0..report.len() {
+                let max_diff = check_diff(report, Some(skip));
+                if max_diff < 4 && max_diff > -4 && max_diff != 0 {
+                    counter += 1;
+                    continue 'outer
+                }
+            }
+        }
+    }
+    
+    println!("part two: {}", counter);
+}
+
+fn check_diff(rep: &Vec<i32>, skip: Option<usize>) -> i32 {
+    let mut clone = rep.clone();
+    if let Some(i) = skip {
+        clone.remove(i);
+    }
+
+    if !(clone.iter().is_sorted() || clone.iter().rev().is_sorted()) {
+        return -5 // death
+    }
+    
+    let mut max_diff = 0;
+
+    for (index, value) in clone.iter().enumerate() {
+        if index + 1 == clone.len() {
+            break
+        }
+
+        let diff = value - clone[index+1];
+        if diff == 0 {
+            max_diff = 0;
+            break
+        }
+        max_diff = max_diff.max(diff.abs()) // since it can be decreasing
+    }
+    
+    max_diff
 }
